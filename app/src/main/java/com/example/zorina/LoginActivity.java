dd9cbbab6636c5a524523e92cf7c765ed10e001c
JonathanA <jonaudris23@gmail.com>
2022-04-03 15:37:48 +0700

    ZorinaLoginRegister

---
+++ app/src/main/java/com/example/zorina/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.example.zorina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void back(View view){
+        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(intent);
+    }
+
+}
